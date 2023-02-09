2278a58321d382b8d17346484ad5e7fc9da001e8
PhamPhucHau <phamphuchauthcsquangtrung@gmail.com>
2023-02-09 12:29:12 +0700

    Update code

--- src/main/java/com/shinhands/mu/Stationary/controller/DiscountProductsController.java
+++ src/main/java/com/shinhands/mu/Stationary/controller/DiscountProductsController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController("")
-@RequestMapping("")
+@RequestMapping("/discounts")
 public class DiscountProductsController {
     @Autowired
     private DiscountProductsService discountProductsService;
