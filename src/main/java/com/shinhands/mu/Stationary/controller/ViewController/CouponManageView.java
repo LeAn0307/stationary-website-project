a018d2a522a1e2a2a84b8ffe6701348530832371
An <lethixuanan2012@gmail.com>
2023-02-10 15:34:46 +0700

    back up

--- src/main/java/com/shinhands/mu/Stationary/controller/ViewController/CouponManageView.java
+++ src/main/java/com/shinhands/mu/Stationary/controller/ViewController/CouponManageView.java
@@ -1,20 +1,26 @@
 package com.shinhands.mu.Stationary.controller.ViewController;
 
 import com.shinhands.mu.Stationary.dto.CategoryDTO;
+import com.shinhands.mu.Stationary.dto.CouponDTO;
+import com.shinhands.mu.Stationary.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping(value = "pages")
 public class CouponManageView {
 
+    @Autowired
+    CouponService couponService;
     @GetMapping(value = "coupon-manage-element")
     public ModelMap CategoryElements(ModelMap model) {
-
-//        CouponDTO[] coupon = new CouponDTO[]{new CouponDTO(1L,"1","img.url"),new CategoryDTO(2L,"1","img.url"),new CategoryDTO(3L,"1","img.url")};
-//        model.addAttribute("categories",categories);
+        List<CouponDTO> coupon =  couponService.getAllCoupons();
+        model.addAttribute("coupon",coupon);
         return model;
     }
 }
