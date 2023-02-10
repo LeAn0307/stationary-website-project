7f2cb799c53cb02b77f160dd48ccfe42fd991917
An <lethixuanan2012@gmail.com>
2023-02-10 14:27:34 +0700

    init

--- src/main/java/com/shinhands/mu/Stationary/controller/ViewController/CouponManageView.java
+++ src/main/java/com/shinhands/mu/Stationary/controller/ViewController/CouponManageView.java
@@ -1,2 +1,20 @@
-package com.shinhands.mu.Stationary.controller.ViewController;public class HomeView {
+package com.shinhands.mu.Stationary.controller.ViewController;
+
+import com.shinhands.mu.Stationary.dto.CategoryDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "pages")
+public class CouponManageView {
+
+    @GetMapping(value = "coupon-manage-element")
+    public ModelMap CategoryElements(ModelMap model) {
+
+//        CouponDTO[] coupon = new CouponDTO[]{new CouponDTO(1L,"1","img.url"),new CategoryDTO(2L,"1","img.url"),new CategoryDTO(3L,"1","img.url")};
+//        model.addAttribute("categories",categories);
+        return model;
+    }
 }
