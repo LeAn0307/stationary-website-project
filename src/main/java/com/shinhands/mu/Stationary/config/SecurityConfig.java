80e2dd4c5b3ba8697b4a6859b748c2556eb4f0cf
luanle13 <thanhluan130201@gmail.com>
2023-02-10 14:33:24 +0700

    jwt token

--- src/main/java/com/shinhands/mu/Stationary/config/SecurityConfig.java
+++
@@ -1,6 +0,0 @@
-package com.shinhands.mu.Stationary.config;
-
-public interface SecurityConfig {
-    boolean isMatch(String rawPassword, String encodePassword);
-    String encode(String password);
-}
