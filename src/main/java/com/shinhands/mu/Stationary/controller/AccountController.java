7510d86c845afe83d8b2571c4df889c1631358e6
An <lethixuanan2012@gmail.com>
2023-02-13 01:05:55 +0700

    Revert "Merge pull request #28 from LeAn0307/luan"
    This reverts commit 5572371205e013a58f56d85d87f8787b1b908c1a, reversing
    changes made to 144a3e37e864a1b5e49c80c2d638abde452283ed.

--- src/main/java/com/shinhands/mu/Stationary/controller/AccountController.java
+++ src/main/java/com/shinhands/mu/Stationary/controller/AccountController.java
@@ -2,9 +2,7 @@ package com.shinhands.mu.Stationary.controller;
 
 import com.shinhands.mu.Stationary.config.security.JwtUtil;
 import com.shinhands.mu.Stationary.dto.AccountDTO;
-import com.shinhands.mu.Stationary.form.RegisterForm;
 import com.shinhands.mu.Stationary.service.AccountService;
-import com.shinhands.mu.Stationary.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -20,16 +18,12 @@ public class AccountController {
     @Autowired
     AccountService accountService;
     @Autowired
-    private UserService _userService;
+    private AuthenticationManager authenticationManager;
     @Autowired
     private JwtUtil jwtTokenUtil;
     @Autowired
     private UserDetailsService userDetailsService;
 
-    @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterForm body) {
-        return null;
-    }
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AccountDTO accountDTO) throws Exception {
         if (accountService.authentication(accountDTO)) {
