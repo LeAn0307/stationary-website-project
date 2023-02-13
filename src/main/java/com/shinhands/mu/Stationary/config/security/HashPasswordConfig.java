1fabc33bf10fa166930729e1e24f319e1591937b
NguyenQuy <darklord0305@gmail.com>
2023-02-13 07:31:20 +0700

    disable security code

--- src/main/java/com/shinhands/mu/Stationary/config/security/HashPasswordConfig.java
+++ src/main/java/com/shinhands/mu/Stationary/config/security/HashPasswordConfig.java
@@ -1,20 +1,20 @@
-package com.shinhands.mu.Stationary.config.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-
-@Configuration
-public class HashPasswordConfig {
-    private String _secret = "mu-shinhands-stationary";
-    private int _iteration = 1000;
-    private int _hashWidth = 128;
-    private Pbkdf2PasswordEncoder _encoder = new Pbkdf2PasswordEncoder(_secret, _iteration, _hashWidth);
-
-    public boolean isMatch(String rawPassword, String encodePassword) {
-        return _encoder.matches(rawPassword, encodePassword);
-    }
-
-    public String encode(String password) {
-        return _encoder.encode(password);
-    }
-}
+//package com.shinhands.mu.Stationary.config.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+//
+//@Configuration
+//public class HashPasswordConfig {
+//    private String _secret = "mu-shinhands-stationary";
+//    private int _iteration = 1000;
+//    private int _hashWidth = 128;
+//    private Pbkdf2PasswordEncoder _encoder = new Pbkdf2PasswordEncoder(_secret, _iteration, _hashWidth);
+//
+//    public boolean isMatch(String rawPassword, String encodePassword) {
+//        return _encoder.matches(rawPassword, encodePassword);
+//    }
+//
+//    public String encode(String password) {
+//        return _encoder.encode(password);
+//    }
+//}
